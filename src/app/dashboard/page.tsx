7b8b176300474dcbebd1a01cc2c5065bda7f5b16
ahwanfebriantoro <ahwanfebrian.dev@gmail.com>
2024-10-13 16:16:01 +0700

    fix: dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,37 +1,14 @@
-"use client";
-import Button from "@/components/Button";
-import TextInput from "@/components/TextInput";
-import Image from "next/image";
-import { useForm } from "react-hook-form";
-import styles from "./dashboard.module.css";
+import Row from "@/components/Grid/Row";
+import ImageUploader from "../tambah-donasi/image-uploader";
+import Col from "@/components/Grid/Col";
 import Link from "next/link";
 
 export default function Dashboard() {
-    const { register, handleSubmit } = useForm();
-    return (
-        <main className={styles.container}>
-            <nav>
-                <div>
-                    <Link href={'/Beranda'}>
-                        Beranda
-                    </Link>
-                </div>
-                <div>
-                    <Link href={'/Donasi'}>
-                        Donasi
-                    </Link>
-                </div>
-                <div>
-                    <Link href={'/Kategori'}>
-                        Kategori
-                    </Link>
-                </div>
-                <div>
-                    <Link href={'/Blog'}>
-                        Blog
-                    </Link>
-                </div>
-            </nav>
-        </main>
-    );
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>Custom / Animasi image di depan</h3>
+      <br></br>
+      <ImageUploader />
+    </div>
+  );
 }
