9923fbcf52e320208c130b1fe1705f2e88eb5aca
ahwanfebriantoro <ahwanfebrian.dev@gmail.com>
2024-10-15 07:36:26 +0700

    add images upload on tambah donasi page

--- src/app/tambah-donasi/image-uploader.tsx
+++ src/app/tambah-donasi/image-uploader.tsx
@@ -4,48 +4,41 @@ import TextInput from "@/components/TextInput";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 import styles from "./tambah-donasi.module.css";
+import { useState } from "react";
+import ImageUpload from "@/components/ImageUpload";
 
 export default function ImageUploader() {
+  const [images, setImages] = useState<any[]>([]);
   return (
     <div style={{ display: "flex", gap: 10 }}>
-      <Image
-        src={"https://picsum.photos/200"}
-        alt="avatar"
-        width={200}
-        height={200}
-        
-        className={styles.image}
-        style={{ aspectRatio: 1 / 1 }}
-      />
-      <Image
-        src={"https://picsum.photos/200"}
-        alt="avatar"
-        width={200}
-        height={200}
-        className={styles.image}
-        style={{ aspectRatio: 1 / 1 }}
-      />
-      <Image
-        src={"https://picsum.photos/200"}
-        alt="avatar"
-        width={200}
-        height={200}
-        className={styles.image}
-        style={{ aspectRatio: 1 / 1 }}
-      />
-      <button
-        className={styles.card}
-        style={{
-          width: 200,
-          height: 200,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          cursor: "pointer",
-        }}
+      {images.map((e) => (
+        <Image
+          key={e}
+          src={e}
+          alt="avatar"
+          width={200}
+          height={200}
+          className={styles.image}
+          style={{ aspectRatio: 1 / 1 }}
+        />
+      ))}
+      <ImageUpload
+        onCropped={(croppedImage) => setImages([...images, croppedImage])}
       >
-        <Image src={"/plus.svg"} width={58} height={58} alt="avatar" />
-      </button>
+        <button
+          className={styles.card}
+          style={{
+            width: 200,
+            height: 200,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+        >
+          <Image src={"/plus.svg"} width={58} height={58} alt="avatar" />
+        </button>
+      </ImageUpload>
     </div>
   );
 }
