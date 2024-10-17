4172885185b92d9d47c5f0ef2ee958c5e505704c
ahwanfebriantoro <ahwanfebrian.dev@gmail.com>
2024-10-17 20:10:36 +0700

    fix breadcrumb

--- src/app/donasi/tambah-donasi/page.tsx
+++ src/app/donasi/tambah-donasi/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 import styles from "./tambah-donasi.module.css";
 import ImageUploader from "./image-uploader";
+import Breadcrumb from "@/components/Breadcrumb";
 
 export default function Profil() {
   return (
     <main className={styles.container}>
       <h1 style={{ textAlign: "center" }}>Tambah Donasi</h1>
-      <p>{"Donor > Tambah Produk"}</p>
-      <div className={styles.card} style={{ display: "flex", flexDirection: "column", flex: 1, gap: 20 }}>
+      <Breadcrumb />
+      <div
+        className={styles.card}
+        style={{ display: "flex", flexDirection: "column", flex: 1, gap: 20 }}
+      >
         <div
           style={{ display: "flex", flexDirection: "column", flex: 1, gap: 20 }}
         >
           <h3>Detail Produk</h3>
-         
         </div>
         <div
           style={{ display: "flex", flexDirection: "column", flex: 2, gap: 20 }}
@@ -26,7 +29,7 @@ export default function Profil() {
             <p>Pakaian</p>
           </div>
           <div className={styles.row}>
-            <b>Nama</b>
+            <b>Deskripsi</b>
             <p>
               Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
               dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit
@@ -38,9 +41,12 @@ export default function Profil() {
           </div>
         </div>
       </div>
-      <div className={styles.card} style={{ display: 'flex',  gap: 20, flexDirection: 'column'}}>
+      <div
+        className={styles.card}
+        style={{ display: "flex", gap: 20, flexDirection: "column" }}
+      >
         <h3>Foto dan Video</h3>
-        <div style={{flex: 1}}>
+        <div style={{ flex: 1 }}>
           <ImageUploader />
         </div>
       </div>
