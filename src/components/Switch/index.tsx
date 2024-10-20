b98e81758729a0bfd2c75605288ee093938da229
ahwanfebriantoro <ahwanfebrian.dev@gmail.com>
2024-10-20 14:04:54 +0700

    switch button default value

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React, { useState } from 'react';
-import styles from './Switch.module.css';
+import React, { useState } from "react";
+import styles from "./Switch.module.css";
 
-const SwitchButton: React.FC = () => {
-  const [isChecked, setIsChecked] = useState(false);
+const SwitchButton = (props: { onChange?: any; isChecked?: boolean }) => {
+  const { isChecked: isCheck = false } = props;
+  const [isChecked, setIsChecked] = useState(isCheck);
 
   const handleToggle = () => {
     setIsChecked(!isChecked);
+    if (props?.onChange) {
+      props.onChange(!isChecked);
+    }
   };
 
   return (
     <label className={styles.switch}>
-      <input 
-        type="checkbox" 
-        checked={isChecked} 
-        onChange={handleToggle} 
-      />
+      <input type="checkbox" checked={isChecked} onChange={handleToggle} />
       <span className={styles.slider}></span>
     </label>
   );
